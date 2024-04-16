897072b229a7ad962aada875f0c133521d3b1ab1
Thanosdamodar <108324506+Thanosdamodar@users.noreply.github.com>
2024-04-16 18:27:44 +0530

    Create band.py
    This python program takes the name of the city your grew up in and the name of your pet and suggests you a Band name that your might be struggling with.

---
+++ band.py
@@ -0,0 +1,7 @@
+##Day 1 :- Band Name Generator
+print('''Welcome to the Band Name Generator
+What's the name of the city you grew up in?''')
+city=input()
+Pet=input("What's your pet's name?\n")
+Band=city+" "+Pet
+print("Your Band name could be",Band)
